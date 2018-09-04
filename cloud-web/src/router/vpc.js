5c8375de3d0b07a61e79197d883823b474b93fe5
bujiaxing <910698124@qq.com>
2018-09-04 15:26:53 +0800

    【删除】专有网络VPC多余页面

--- cloud-web/src/router/vpc.js
+++ cloud-web/src/router/vpc.js
@@ -3,7 +3,7 @@ const pnDetail = () => import('@/views/vpc/pn/Detail.vue');
 const pnSubnet = () => import('@/views/vpc/pn/Subnet.vue');
 const Flexip = () => import('@/views/vpc/pn/Flexip.vue');
 const RouteManage = () => import('@/views/vpc/routeManage/RouteManage.vue');
-const vfirewall = () => import('@/views/vpc/vfirewall/Index.vue');
+// const vfirewall = () => import('@/views/vpc/vfirewall/Index.vue');
 export default [
     {
         path: 'vpc',
@@ -42,42 +42,6 @@ export default [
                 path: 'pn/routeManage',
                 name: 'app.vpc.pn-routeManage', // 路由管理
                 component: RouteManage
-            },
-            {
-                path: 'vfirewall',
-                name: 'app.vpc.vfirewall',
-                redirect: '/people/vpc/vfirewall/list',
-                component: {
-                    render(c) {
-                        return c('router-view');
-                    }
-                },
-                children: [
-                    {
-                        path: 'list',
-                        name: 'app.vpc.vfirewall.list',
-                        component: vfirewall,
-                        meta: {
-                            parentName: 'app.vpc'
-                        }
-                    }
-                ]
-            },
-            {
-                path: 'peerconn',
-                name: 'app.vpc.peerconn',
-                redirect: '/people/vpc/peerconn/list',
-                component: () => import('@/views/vpc/peerconn/Peerconn.vue'),
-                children: [
-                    {
-                        path: 'list',
-                        name: 'app.vpc.peerconn.list',
-                        component: () => import('@/views/vpc/peerconn/List.vue'),
-                        meta: {
-                            parentName: 'app.vpc'
-                        }
-                    }
-                ]
             }
         ]
     }
