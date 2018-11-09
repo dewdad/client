98cd1f55dfb8366d4a1999ed8f0004fac869c447
文芳 <wenfang@szzt.com.cn>
2018-11-09 16:02:29 +0800

    创建网络增加IPV6类型输入

--- cloud-web/src/views/vpc/pn/Create.vue
+++ cloud-web/src/views/vpc/pn/Create.vue
@@ -1,57 +1,64 @@
 <template>
-<el-dialog :visible.sync="isShow" v-loading="loading" loading="empty">
-    <div slot="title">{{title}}</div>
-    <div class="pr130">
-        <zt-form size="small" :model="data" :rules="rules" ref="form" label-width="140px" inline-message>
-            <zt-form-item label="专有网络名称" prop="name">
-                <el-input v-model="data.name" maxlength="48"></el-input>
-                <span slot="help" class="input-help">只能由中文、英文字母、数字、下划线、中划线组成，长度小于48个字符。</span>
-            </zt-form-item>
-            <zt-form-item v-if="this.type === 'create'" label="网段" prop="cindr">
-                <ip-input v-model="data.cindr" :value="cindrVal" v-if="isShow"></ip-input>
-                <span slot="help" class="input-help">
-                    <span class="color-warning lh20">创建后无法修改。</span><br>
-                    <span class="lh20">例如：192.168.94.0/24</span>
-                </span>
-            </zt-form-item>
-            <zt-form-item v-if="this.type === 'create'" label="IP版本">
-                <el-radio-group v-model="data.version">
-                    <el-radio :label="4">IPV4</el-radio>
-                    <el-radio :label="6">IPV6</el-radio>
-                </el-radio-group>
-            </zt-form-item>
-            <zt-form-item v-if="this.type === 'create'" label="DHCP">
-                <el-radio-group v-model="data.DHCPVal">
-                    <el-radio :label="true">已激活</el-radio>
-                    <el-radio :label="false">未激活</el-radio>
-                </el-radio-group>
-            </zt-form-item>
-             <zt-form-item v-if="this.type === 'create'" label="设置网关">
-                <el-radio-group v-model="data.setGateway">
-                    <el-radio :label="true">是</el-radio>
-                    <el-radio :label="false">否</el-radio>
-                </el-radio-group>
-            </zt-form-item>
-             <zt-form-item v-if="data.setGateway" label="外部网关" prop="externalGateway" :rules="[{required: true, message: '请选择外部网关', trigger: ['blur', 'change']}]">
-                <el-select v-model="data.externalGateway" placeholder="请选择外部网关">
-                    <el-option v-for="item in netWorkList" :key="item.id" :value="item.id" :label="item.name">{{item.name}}</el-option>
-                </el-select>
-            </zt-form-item>
-        </zt-form>
-    </div>
-    <span slot="footer" class="dialog-footer">
-        <el-button type="info" size="small" @click="hide">取 消</el-button>
-        <el-button type="primary" size="small" @click="confirm" :loading="loading">确 定</el-button>
-    </span>
-</el-dialog>
+    <el-dialog :visible.sync="isShow" v-loading="loading" loading="empty">
+        <div slot="title">{{title}}</div>
+        <div class="pr130">
+            <zt-form size="small" :model="data" :rules="rules" ref="form" label-width="140px" inline-message>
+                <zt-form-item label="专有网络名称" prop="name">
+                    <el-input v-model="data.name" maxlength="48"></el-input>
+                    <span slot="help" class="input-help">只能由中文、英文字母、数字、下划线、中划线组成，长度小于48个字符。</span>
+                </zt-form-item>
+                <zt-form-item v-if="this.type === 'create'" label="IP版本">
+                    <el-radio-group v-model="data.version">
+                        <el-radio :label="4">IPV4</el-radio>
+                        <el-radio :label="6">IPV6</el-radio>
+                    </el-radio-group>
+                </zt-form-item>
+                <zt-form-item v-if="this.type === 'create' && data.version === 4" key="IPV4" label="网段" prop="cindr">
+                    <ip-input v-model="data.cindr" :value="cindrVal" v-if="isShow"></ip-input>
+                    <span slot="help" class="input-help">
+                        <span class="color-warning lh20">创建后无法修改。</span><br>
+                        <span class="lh20">例如：192.168.94.0/24</span>
+                    </span>
+                </zt-form-item>
+                <zt-form-item v-if="this.type === 'create' && data.version === 6" key="IPV6" label="网段" prop="cindr">
+                    <el-input v-model="data.cindr" maxlength="50"></el-input>
+                    <span slot="help" class="input-help">
+                        <span class="color-warning lh20">创建后无法修改。</span><br>
+                        <span class="lh20">例如：0:0:0:0:0:0:0:1/48</span>
+                    </span>
+                </zt-form-item>
+                <zt-form-item v-if="this.type === 'create'" label="DHCP">
+                    <el-radio-group v-model="data.DHCPVal">
+                        <el-radio :label="true">已激活</el-radio>
+                        <el-radio :label="false">未激活</el-radio>
+                    </el-radio-group>
+                </zt-form-item>
+                <zt-form-item v-if="this.type === 'create'" label="设置网关">
+                    <el-radio-group v-model="data.setGateway">
+                        <el-radio :label="true">是</el-radio>
+                        <el-radio :label="false">否</el-radio>
+                    </el-radio-group>
+                </zt-form-item>
+                <zt-form-item v-if="data.setGateway" label="外部网关" prop="externalGateway" :rules="[{required: true, message: '请选择外部网关', trigger: ['blur', 'change']}]">
+                    <el-select v-model="data.externalGateway" placeholder="请选择外部网关">
+                        <el-option v-for="item in netWorkList" :key="item.id" :value="item.id" :label="item.name">{{item.name}}</el-option>
+                    </el-select>
+                </zt-form-item>
+            </zt-form>
+        </div>
+        <span slot="footer" class="dialog-footer">
+            <el-button type="info" size="small" @click="hide">取 消</el-button>
+            <el-button type="primary" size="small" @click="confirm" :loading="loading">确 定</el-button>
+        </span>
+    </el-dialog>
 </template>
 
 <script>
 import {createNetwork, updateNetwork} from '@/service/v2.1/network.js';
-import { queryNetwork} from '@/service/ecs/network.js';
+import {queryNetwork} from '@/service/ecs/network.js';
 import IpInput from '@/components/form/IPInput.vue';
 
-function judgeSubnetIpValid(ip ,mask) {
+function judgeSubnetIpValid(ip, mask) {
     //子网网段必须属于下面三类：10.0.0.0/8~28，172.16.0.0/12~28，192.168.0.0/16~28。
     var ip_part0 = parseInt(ip[0]);
     var ip_part1 = parseInt(ip[1]);
@@ -78,16 +85,20 @@ function judgeSubnetIpValid(ip ,mask) {
         return false;
     }
 }
+
 export default {
     data() {
         // 验证 IP 格式
-        let cindr = function(rule, value, callback) {
+        const cindr = (rule, value, callback) => {
             if (value === '' || value === '.../') {
                 callback(new Error('请输入网段'));
             } else {
                 let [ipstr, port] = value.split('/');
                 let ip = ipstr.split('.');
-                if (port && ip.length === 4 && ip.every(e => e >= 0) && judgeSubnetIpValid(ip, port)) {
+                if (
+                    (this.data.version === 4 && port && ip.length === 4 && ip.every(e => e >= 0) && judgeSubnetIpValid(ip, port)) ||
+                    (this.data.version === 6 && value.length <= 50 && /^[a-z0-9]+(:[a-z0-9]+)+\/\d+$/.test(value))
+                ) {
                     callback();
                 } else {
                     callback(new Error('格式不正确'));
@@ -110,18 +121,15 @@ export default {
             },
             rules: {
                 name: [
-                    { required: true, message: '请输入专有网络名称', trigger: 'blur' },
-                    { min: 1, max: 48, message: '长度为2-48个字符', trigger: 'blur' },
+                    {required: true, message: '请输入专有网络名称', trigger: 'blur'},
+                    {min: 1, max: 48, message: '长度为2-48个字符', trigger: 'blur'},
                     {
                         pattern: /^[\u4e00-\u9fa5a-zA-Z0-9_-]*$/,
                         message: '专有网络名称格式错误',
                         trigger: 'blur'
                     }
                 ],
-                cindr: [
-                    { required: true, message: '请输入网段', trigger: 'blur' },
-                    { validator: cindr, trigger: 'submit' }
-                ],
+                cindr: [{required: true, message: '请输入网段', trigger: 'blur'}, {validator: cindr, trigger: 'submit'}]
             },
             resolve: () => {},
             reject: () => {},
@@ -138,7 +146,7 @@ export default {
     },
     watch: {
         isShow(val) {
-            if(!val) {
+            if (!val) {
                 this.data.name = '默认专有网络';
                 this.$refs.form.resetFields();
             }
@@ -185,7 +193,7 @@ export default {
                 subnet: {
                     cidr: this.data.cindr,
                     dHCPEnabled: this.data.DHCPVal,
-                    ipVersion: this.data.version,
+                    ipVersion: this.data.version
                 }
             };
             if (this.data.setGateway) {
@@ -210,7 +218,7 @@ export default {
             // let params = Object.assign({}, this.data);
             let params = {
                 id: this.row.id,
-                name: this.data.name,
+                name: this.data.name
             };
             this.loading = true;
             updateNetwork({...params})
