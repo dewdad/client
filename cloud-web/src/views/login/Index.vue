93973c0a1679c63a2a03a7036fdfa42a9302db69
文芳 <wenfang@szzt.com.cn>
2018-11-09 15:01:37 +0800

    登录页面增加申请试用链接

--- cloud-web/src/views/login/Index.vue
+++ cloud-web/src/views/login/Index.vue
@@ -14,8 +14,11 @@
                     <div class="logo"><img src="@/assets/images/logo.png"></div>
                     <div class="login-box--form">
                         <el-alert v-if="errorMsg !==''" :title="errorMsg" class="mb20" type="error" :closable="false" show-icon>
-                                </el-alert>
+                        </el-alert>
                         <el-form autocomplete="off" ref="pwdForm" :model="user" label-width="0" v-loading="isLoging" :rules="rules" element-loading-spinner="el-icon-loading" :show-message="false">
+                            <el-form-item label="" class="mb10">
+                               您可以申请试用获取体验账户，<a href="http://cloud.szzt.com.cn/#/register/reg" target="_blank">立即申请</a>
+                            </el-form-item>
                             <el-form-item label="" prop="userName" :class="{'is-nomal': errorInput === ''}">
                                 <input type="text" id="username" style="display: none;" disabled autocomplete="off" />
                                 <input type="password" id="password" style="display: none;" disabled autocomplete="off" />
