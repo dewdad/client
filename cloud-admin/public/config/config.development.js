2ac97fd16de92c9a3d65b9275d6fab0dc96d268a
欧阳宏武 <ouyanghongwu@szzt.com.cn>
2018-11-14 12:09:57 +0800

    Update config.development.js

--- cloud-admin/public/config/config.development.js
+++ cloud-admin/public/config/config.development.js
@@ -1,2 +1,2 @@
-var API_URL = 'http://10.11.3.101:7100/'; // http://192.168.1.73:9000
+var API_URL = 'http://10.11.3.99:7100/'; // http://192.168.1.73:9000
 var FILE_URL = API_URL + '/services-user/file/';
